64d55464fe07b802ecf131e1eaa9b2acd3e0cfd7
Lawrence137 <lawrencekimani18@gmail.com>
2022-11-24 11:27:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
+
+
+let random = Math.floor(Math.random() * 8);
+
+
+let num3 = 38;
+let num4 = 34;
+let mod = num3 % num4;
+
+
+let max = Math.max(2, 8, 13, 17, 20, 11);
